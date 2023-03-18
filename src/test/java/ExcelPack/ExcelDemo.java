ffcbb745d22024fdfe4b587ddc36aa7dd7f6b02a
merkurt2001 <merkurt2001@gmail.com>
2023-03-18 01:08:13 +0100

    excel Util added

---
+++ src/test/java/ExcelPack/ExcelDemo.java
@@ -0,0 +1,43 @@
+package ExcelPack;
+
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ExcelDemo {
+
+    @Test
+    public void excelRead() throws IOException {
+        FileInputStream fileInputStream= new FileInputStream("Vytracktestdata.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet sheet = workbook.getSheet("QA3-short");
+
+        int rowNum = sheet.getLastRowNum();
+        System.out.println(rowNum);
+        System.out.println(sheet.getRow(3).getCell(3));
+    }
+
+
+    @Test
+    public void excelWrite() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("Vytracktestdata.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet sheet = workbook.getSheet("QA3-short");
+
+        System.out.println("sheet.getRow(2).getCell(2) = " + sheet.getRow(2).getCell(2));
+
+        sheet.getRow(2).getCell(2).setCellValue("Max");
+
+        System.out.println("sheet.getRow(2).getCell(2) = " + sheet.getRow(2).getCell(2));
+
+        FileOutputStream fileOutputStream = new FileOutputStream("Vytracktestdata.xlsx");
+        workbook.write(fileOutputStream);
+
+
+    }
+}
