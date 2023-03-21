a92bf3fe66f41663925986ab4f8e4b6f6df42265
merkurt2001 <merkurt2001@gmail.com>
2023-03-21 22:55:21 +0100

    robot class demo added

--- src/test/java/InterstingInfo/Pagination.java
+++ src/test/java/InterstingInfo/Pagination.java
@@ -3,12 +3,12 @@ package InterstingInfo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import utilities.BrowserUtils;
 import utilities.Driver;
 
 import java.time.Duration;
 
 public class Pagination {
-
 
     @Test
     public void performanceTest() throws InterruptedException {
@@ -18,40 +18,75 @@ public class Pagination {
         Driver.getDriver().get("https://qa-recruitment-task.netlify.app/");
 
 
-        boolean flag = true;
-        String isNextPage = "";
-        String isPreviousPage = "";
         WebElement nextButton = Driver.getDriver().findElement(By.xpath("(//*[name()='svg'][@class='icon'])[16]"));
         WebElement previousButton = Driver.getDriver().findElement(By.xpath("(//*[name()='svg'][@class='icon'])[15]"));
 
-        while (flag) {
+        while (true) {
+            BrowserUtils.sleep(1);
             nextButton.click();
-            try {
-                isNextPage = Driver.getDriver().findElement(By.cssSelector(".PaginationIndicatorstyle__Li-urm45v-1.dJyhIL")).getAttribute("disabled");
-            } catch (Exception e) {
-                System.out.println("not inside last page");
-            }
 
-            if (isNextPage.equals("true")) {
-                flag = false;
+            String isLastPage = Driver.getDriver().findElement(By.xpath("//li[starts-with(@class,\"PaginationIndicatorstyle__Li\")][last()]")).getAttribute("disabled");
+
+            if (isLastPage == null) {
+                System.out.println("not at the last page");
+            } else if (isLastPage.equals("true")) {
+                System.out.println("LAST PAGE");
+                break;
             }
         }
-        flag = true;
-        while (flag) {
-            previousButton.click();
-            try {
-                isPreviousPage = Driver.getDriver().findElement(By.cssSelector(".PaginationIndicatorstyle__Li-urm45v-1.ZquGu")).getAttribute("disabled");
-            } catch (Exception e) {
-                System.out.println("not inside first page");
-            }
 
-            if (isPreviousPage.equals("true")) {
-                flag = false;
+        while (true) {
+            BrowserUtils.sleep(1);
+            previousButton.click();
+
+            String isFirstPage = Driver.getDriver().findElement(By.xpath("//li[starts-with(@class,\"PaginationIndicatorstyle__Li\")][1]")).getAttribute("disabled");
+
+            if (isFirstPage == null) {
+                System.out.println("not at the first page");
+            } else if (isFirstPage.equals("true")) {
+                System.out.println("FIRST PAGE");
+                break;
             }
         }
 
 
     }
 
+    @Test
+    public void pagination2() {
+
+
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Driver.getDriver().get("https://qa-recruitment-task.netlify.app/");
+
+
+        WebElement forwardPaginationIcon = Driver.getDriver().findElement(By.xpath("//li[@class='PaginationIndicatorstyle__Li-urm45v-1 MCnLo']"));
+
+        int count = 0;
+
+        while (true) {
+
+            forwardPaginationIcon.click();
+            count++;
+            try {
+                BrowserUtils.sleep(1);
+                Driver.getDriver().findElement(By.xpath("//li[@class='PaginationIndicatorstyle__Li-urm45v-1 MCnLo']"));
+
+            } catch (Exception e) {
+                System.out.println("No more visible");
+                break;
+            }
+        }
+
+        WebElement backPaginationIcon = Driver.getDriver().findElement(By.xpath("(//li[@class='PaginationIndicatorstyle__Li-urm45v-1 jpDQtp'])[1]"));
+
+        while (count > 0) {
+            BrowserUtils.sleep(1);
+            backPaginationIcon.click();
+            count--;
+
+        }
+    }
 
 }
