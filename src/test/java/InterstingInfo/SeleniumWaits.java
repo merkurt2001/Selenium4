cdb6954377f4ff5214be9d5b2a290bdf3d1068db
merkurt2001 <merkurt2001@gmail.com>
2023-10-17 17:42:15 +0200

    selenium waits added

--- src/test/java/InterstingInfo/SeleniumWaits.java
+++ src/test/java/InterstingInfo/SeleniumWaits.java
@@ -15,7 +15,7 @@ import java.time.Duration;
 
 public class SeleniumWaits extends TestBase {
     @Test
-    public void implicitWait(){
+    public void implicitWait1() {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
         driver.findElement(By.id("adder")).click();
@@ -24,7 +24,30 @@ public class SeleniumWaits extends TestBase {
     }
 
     @Test
-    public void explicit(){
+    public void implicitWait2() {
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        //waits for the page to load for a specified number of seconds
+//pageLoadTimeout command is essential in performance-related testing.
+        driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
+        driver.findElement(By.id("adder")).click();
+        WebElement added = driver.findElement(By.id("box0"));
+
+    }
+
+    @Test
+    public void implicitWait3() {
+
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+
+        driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
+        driver.findElement(By.id("adder")).click();
+        WebElement added = driver.findElement(By.id("box0"));
+
+    }
+
+    @Test
+    public void explicit() {
         driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
         WebElement revealed = driver.findElement(By.id("revealed"));
         Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(2));
@@ -65,6 +88,25 @@ public class SeleniumWaits extends TestBase {
 
         revealed.sendKeys("Displayed");
         Assertions.assertEquals("Displayed", revealed.getDomProperty("value"));
+
+    }
+
+    @Test
+    public void explicit3() {
+        driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
+        WebElement reveal = driver.findElement(By.id("reveal"));
+        reveal.click();
+
+        try {
+            WebElement revealed = new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOf(driver.findElement(By.id("revealed"))));
+            revealed.sendKeys("Hello");
+            Assertions.assertEquals("Hello", revealed.getDomProperty("value"));
+        } catch (Exception e) {
+            System.out.println("element does not exist");
+        } finally {
+            driver.quit();
+        }
+
 
     }
 }
